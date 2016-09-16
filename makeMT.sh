abbf33969fa5d056791d5681ff2dfe1da5e3f99a
easyw <easyw@katamail.com>
2016-09-16 23:54:22 +0200

    MT for 64bits

--- makeMT.sh
+++ makeMT.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # this shell script make all releases of cxx
 # maui
-make clean
+#rm ccx2_11_MT
+make -f Makefile-211-MT clean
 cp SPOOLES.2.2/Make_MT.inc SPOOLES.2.2/Make.inc && ls 
 make -f Makefile-211-MT && cp CalculiX/ccx_2.11/src/ccx_2.11_MT ccx2_11_MT && ls 
 cp SPOOLES.2.2/Make_base.inc SPOOLES.2.2/Make.inc && ls 
-rm ccx
--- makeall.sh
+++ makeall.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # this shell script make all releases of cxx
 # maui
+# make -f Makefile-208 clean
 make -f Makefile-208 && cp ccx ccx2_08p2 && ls 
+make -f Makefile-210 clean
 make -f Makefile-210 && cp ccx ccx2_10 && ls 
+make -f Makefile-211 clean
 make -f Makefile-211 && cp ccx ccx2_11 && ls 
 # rm ccx && ls 
-./makeMT.sh
+sh ./makeMT.sh
